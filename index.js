63893d5e5180a3fc66cf8b5d9478d4db123cf8ef
Zain <zainrasoolhashmi@gmail.com>
2024-12-30 23:09:00 +0500

    Test collab commit

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ const app = express();
 const PORT = 5000;
 
 //middlewares
-app.use(express.json()); //parse json 
-app.use(morgan('dev'));
+app.use(express.json()); //parse json
+app.use(morgan("dev"));
 app.use(cors());
 app.use(errorHandler());
 
 //imported routes.
-
+// random commit thing
 
 // app.use("/",); //use the router for functionality
 
-
-
 app.listen(PORT, () => {
-      console.log("Connected to TouristPlanner");
-  });
+  console.log("Connected to TouristPlanner");
+});
