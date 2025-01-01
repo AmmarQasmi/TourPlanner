9d3050f1ff71e5dfc124b067cc147a35122b8627
Ammar Qasmi <ammarsqasmi@gmail.com>
2025-01-01 10:11:37 +0500

    ..

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 
 dotenv.config();
-const supabaseUrl = 'https://czcfgyklepicvfdccsco.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(process.env.supabaseUrl, process.env.apikey);
