db1ceac6e63ea44f834f207f60155d4f15ac646a
Ammar Qasmi <ammarsqasmi@gmail.com>
2025-01-01 10:32:42 +0500

    Agents CRUD

---
+++ Backend/controller/clientController.js
@@ -0,0 +1,11 @@
+import { supabase } from '../db/connect.js';
+
+export const getAllClients = async (req,res) => {};
+
+export const getClientById = async (req,res) => {};
+
+export const createClient = async (req,res) => {};
+
+export const updateClient = async (req,res) => {};
+
+export const deleteClient = async (req,res) => {};
