64d08b12b9ecc355aa63e310c20251c035775c31
Zain4391 <manutdfast91@gmail.com>
2025-01-01 10:41:14 +0500

    Client get done

--- Backend/controller/clientController.js
+++ Backend/controller/clientController.js
@@ -1,6 +1,20 @@
 import { supabase } from '../db/connect.js';
 
-export const getAllClients = async (req,res) => {};
+// returns with order desc wrt to time
+export const getAllClients = async (req,res) => {
+    try {
+        const { data, error } = await supabase.from().select('*').order('created_at',{ascending: false});
+
+        if( error ) {
+            return res.status(400).json({message: error.message, Error: true});
+        }
+    
+        return res.status(200).json({message: 'Client found', Clients: data});
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({message: 'Internal server error', Error: true});
+    }
+};
 
 export const getClientById = async (req,res) => {};
 
