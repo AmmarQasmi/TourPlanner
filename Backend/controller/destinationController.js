f348b55b2136489ac92011c58447052e39c79267
Zain4391 <manutdfast91@gmail.com>
2025-01-13 21:19:40 +0500

    dest crud complete

--- Backend/controller/destinationController.js
+++ Backend/controller/destinationController.js
@@ -55,9 +55,65 @@ export const getAllDestinations = async (req, res) => {
       });
     }
   };
-export const createDestination = async (req,res) => {};
+export const createDestination = async (req,res) => {
+  const {name, country, description, cost,image, days} = req.body;
+  try {
 
-export const updateDestination = async (req,res) => {};
+    if(!name || !country || !description || !cost || !image || !days) {
+      return res.status(400).json({message: 'All fields are required', Error: true});
+    }
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const obj = {
+      destination_id: id,
+      name,
+      country,
+      description,
+      average_cost: cost,
+      image,
+      days,
+    }
+    const {data, error} = await supabase.from('destinations').insert(obj);
+    if(error) {
+      return res.status(500).json({message: error.message, Error: true, destinations: []});
+    }
+    return res.status(201).json({message: 'Destination created successfully', Error: false, destinations: data});
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({message: 'Internal server error', Error: true, destinations: []});
+  }
+};
+
+export const updateDestination = async (req,res) => {
+  const {name, country, description, cost,image, days} = req.body;
+  const {destId} = req.params;
+  try {
+    let updateObj = {};
+    if(name) updateObj.name = name;
+    if(country) updateObj.country = country;
+    if(description) updateObj.description = description;
+    if(cost) updateObj.average_cost = cost;
+    if(image) updateObj.image = image;
+    if(days) updateObj.days = days;
+
+    updateObj = {
+      name: name,
+      country: country,
+      description: description,
+      average_cost: cost,
+      image: image,
+      days: days,
+    }
+
+    const {data, error} = await supabase.from('destinations').update(updateObj).eq('destination_id', destId).select();
+    if(error) {
+      return res.status(500).json({message: error.message, Error: true, destinations: []});
+    }
+    return res.status(200).json({message: 'Destination updated successfully', Error: false, destinations: data});
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({message: 'Internal server error', Error: true, destinations: []});
+  }
+};
 
 export const deleteDestination = async (req,res) => {
     const { destId } = req.params;
